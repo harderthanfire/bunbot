a27097ac9ef4e2e4a5b37d3b49fe73d52f1dd1c9
harderthanfire <harderthanfire@gmail.com>
2021-08-06 13:04:49 +0100

    Fix missing return when no matching character on mee command

--- commands/mee.js
+++ commands/mee.js
@@ -10,6 +10,7 @@ module.exports = async function (client, interaction, config) {
 
     if (!char) {
         require("../utils/sendTextReply.js")(client, interaction, "Please register your character using /register!");
+        return;
     }
 
     const url = config.characterUrl + "characters/name/" + char.server + "/" + char.name.replace(" ", "%20") + ".png";
